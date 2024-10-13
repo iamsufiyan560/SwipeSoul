f3c0fb6be5a79206d411bc8c0bdb3272cae34251
iamsufiyan560 <msufiyanhusen@gmail.com>
2024-10-13 20:41:26 +0530

    create authentication api

---
+++ backend/controllers/authController.js
@@ -0,0 +1,117 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+export const signup = async (req, res) => {
+  const { name, email, password, age, gender, genderPreference } = req.body;
+  try {
+    if (!name || !email || !password || !age || !gender || !genderPreference) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    if (age < 18) {
+      return res.status(400).json({
+        success: false,
+        message: "You must at lest 18 years old",
+      });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "Password must be at least 6 characters",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return login(req, res);
+    }
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+      age,
+      gender,
+      genderPreference,
+    });
+
+    const token = signToken(newUser._id);
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Account created successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    console.log("Error in signup controller:", error);
+
+    res.status(500).json({
+      success: false,
+      message:
+        "message: 'An error occurred during signup. Please try again later.',",
+    });
+  }
+};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = signToken(user._id);
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Login successfully",
+      user,
+    });
+  } catch (error) {
+    console.log("Error in login controller:", error);
+    res.status(500).json({
+      success: false,
+      message:
+        "message: 'An error occurred during Login. Please try again later.',",
+    });
+  }
+};
+export const logout = async (req, res) => {
+  res.clearCookie("jwt");
+  res.status(200).json({ success: true, message: "Logged out successfully" });
+};
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+// routes
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server started at this port:" + PORT);
+  connectDB();
+});

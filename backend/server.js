bf5586c6e608bf7b3ec819843814a2e858a31e41
iamsufiyan560 <msufiyanhusen@gmail.com>
2024-10-19 19:19:09 +0530

    deployment setup

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,15 @@ import messageRoutes from "./routes/messageRoutes.js";
 import { connectDB } from "./config/db.js";
 
 import { initializeSocket } from "./socket/socket.server.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const httpServer = createServer(app);
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 initializeSocket(httpServer);
 
@@ -35,6 +38,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/matches", matchRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 httpServer.listen(PORT, () => {
   console.log("Server started at this port:" + PORT);
